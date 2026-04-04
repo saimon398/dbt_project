6cbe3662c5d30dd82c0374ec6db9772285d17d0b
Алексей Попов <aleksborislavskij@gmail.com>
2026-04-04 12:44:38 +0300

    FEAT: добавление новой модели в dm

---
+++ models/scripts/dm/songs_aggregation.sql
@@ -0,0 +1,14 @@
+select 
+	s.primary_genre 		 as primary_genre,
+	s.songs_count 			 as songs_count,
+	m.artist 				 as artist,
+	m.song_title 			 as most_popular_song_title,
+	m.total_streams_billions as total_streams_billions
+from 
+	{{ref('songs_per_genre')}} as s
+	inner join 
+	{{ref("most_popular_song_per_genre")}} as m 
+	on 
+	s.primary_genre = m.primary_genre
+order by 
+	s.songs_count desc
