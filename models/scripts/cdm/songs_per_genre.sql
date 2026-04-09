32889caa06d0e280b0563e54d61316f205f0279f
Алексей Попов <aleksborislavskij@gmail.com>
2026-04-09 20:05:01 +0300

    TEST: проверка работы pre-commit на модели

--- models/scripts/cdm/songs_per_genre.sql
+++ models/scripts/cdm/songs_per_genre.sql
@@ -1,9 +1,9 @@
-select 
-	primary_genre,
-	cast(count(1) as smallint) as songs_count
-from 
-	{{source('ods', 'songs')}}
-group by 
-	primary_genre
-order by 
-	songs_count desc
+SELECT 
+    primary_genre,
+    cast(count(1) AS smallint) AS songs_count
+FROM 
+    {{source('ods', 'songs')}}
+GROUP BY 
+    primary_genre
+ORDER BY 
+    songs_count DESC
