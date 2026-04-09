c5c4d7e72327fbca50f26d54bca42610bd3e4305
Алексей Попов <aleksborislavskij@gmail.com>
2026-04-09 19:57:17 +0300

    TEST: проверка работы pre-commit на модели

--- models/scripts/cdm/most_popular_song_per_genre.sql
+++ models/scripts/cdm/most_popular_song_per_genre.sql
@@ -1,10 +1,10 @@
-select distinct on (primary_genre)
-	primary_genre,
-	song_title,
-	artist,
-	total_streams_billions
-from 
-	{{source('ods', 'songs')}}
-order by 
-	primary_genre,
-	total_streams_billions desc
+SELECT DISTINCT ON (primary_genre)
+    primary_genre,
+    song_title,
+    artist,
+    total_streams_billions
+FROM 
+    {{source('ods', 'songs')}}
+ORDER BY 
+    primary_genre,
+    total_streams_billions DESC
