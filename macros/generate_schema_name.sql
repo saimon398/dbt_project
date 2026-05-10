2edd2ba5534ec555a45ab6e3b151cc9936ce8858
Алексей Попов <aleksborislavskij@gmail.com>
2026-05-10 17:09:01 +0300

    FEAT: добавление Slim CI подхода

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,13 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
     {% set default_schema = target.schema %}
-    {% if target.name == 'dev' or target.name == 'prod' %}
+    {% if target.name == 'prod' %}
         {{ custom_schema_name | trim }}
+    {% elif target.name == 'dev' %}
+        {% if default_schema == 'dbt_objects' %}
+            {{ custom_schema_name | trim }}
+        {% else %}
+            {{ default_schema | trim }}
+        {% endif %}
     {% else %}
         {{ default_schema }}_{{ custom_schema_name | trim }}
     {% endif %}
